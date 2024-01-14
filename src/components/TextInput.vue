e635b2b0c7dc83b58dea7efe449311b5c540bb53
Aya Okada <ayaokada@AyanoMacBook-Air.local>
2024-01-15 05:59:16 +0900

    finish TextInput

--- src/components/TextInput.vue
+++ src/components/TextInput.vue
@@ -1,9 +1,20 @@
 <template>
-  
+  <div>
+    <label for="inputText">文字を入力して下さい〜</label>
+    <input type="text" id="inputText" v-model="inputData">
+    <p>入力された文字：{{ inputData }}</p>
+  </div>
 </template>
 
 <!-- Option API -->
 <script>
+export default {
+  data() {
+    return {
+      inputData:'',
+    };
+  },
+};
 </script>
 
 <!-- Composition API NEW -->
